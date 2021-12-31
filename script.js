6f55cebb8ec973b3733b2e76c81080b799b81fe7
David <dvd.cnt@gmail.com>
2021-12-31 17:33:25 -0600

    Removed webworker
    Removed webworker
    Enhanced results

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const Input = document.getElementById("Input")
 const Results = document.getElementById("Results")
 const wait = 200;
-const firstResponse = ["Type on the input to get results","Or not"]
+const firstResponse = ["Type on the input to get results"]
 
 math.import({props, HAprops, phase})
 const parser = self.math.parser()
