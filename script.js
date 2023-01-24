1bfc2e13b4da481998bc9d49ad7a53e1097f4364
David Contreras <dvd.cnt@gmail.com>
2023-01-24 12:02:34 -0600

    Implemented broadcasting
    There is still work to do by using math.type instead of manually checking for types

--- script.js
+++ script.js
@@ -23,19 +23,117 @@ const example = [
     "HAprops('T', {P:1 atm, H:h, R:1.0})"
 ]
 
-// this function return a mapped function in case of array or the function in case of scalar
+// maps a function if needed
 function mapped(f, x) {
   return math.sum(math.size(x)) > 0 ? math.map(x, f) : f(x)
 }
 
+// maps a function if for the case of log
 function mapLog(x, ...args) {
   const base = args.length == 0 ? math.e : args[0]
   return math.sum(math.size(x)) > 0 ? math.map(x, x => math.log(x, base)) : math.log(x, base)
 }
 
-const mat = math.create(math.all)
+// consistently gets the size of an array, matrix or scalar as an array
+sizeAsArray = (x) => (math.isMatrix(x) | isScalar(x)) ? math.size(x).toArray() : math.size(x)
+
+// checks if a value is scalar
+isScalar = (x) => math.count(math.size(x)) == 0
+
+// checks if the size of a value corresponds to a scalar
+sizeOfScalar = (size) => math.sum(math.size(size)) == 0
+
+// creates shape of the dimension needed to concat a value in N dimensions
+// a matrix of size [3, 4] to dim = 3 yields [1, 3, 3]
+// in other words it appends 1 to the left as needed
+shapeToConcat = (Mat, dim) => math.resize(sizeAsArray(Mat).reverse(), [dim], 1).reverse()
+
+// calculates what is the common sizes/shapes of the functions to concat
+// throws an error specifying why the sizes/shapes can't be broadcasted
+function broadcast_shapes(...shapes) {
+  // is the number of dims of the shapes to broadcast
+  const sizes = shapes.map(shape => shape.length)
+  // it should work with scalars but it doesn't
+  const N = math.max(sizes)
+  let broadcasted_shape = []
+  let maxDims = []
+
+  // Gets the maximum size in each dimension
+  for (let n = 0; n < N; ++n) {
+    let maxDim = 0
+    shapes.forEach((shape, ns) => {
+      if (sizes[ns] > n) {
+        const nDim = shape[sizes[ns] - n - 1]
+        if (nDim > maxDim) {
+          maxDim = nDim
+        }
+      }
+    })
+    maxDims[N - n - 1] = maxDim
+  }
+
+  // Calculates the broadcasted shape
+  // If for each shape to broadcast, check from right to left
+  // from last dimension to first dimension if it can be broadcasted
+  // It means it is possible to broadcast a dimension to a dimension N
+  // the current dimension is none, 1
+  // if the current dimension is already N there is nothing to be done
+  // it is not possible to broadcast a dimension size 2 to a 3 for example
+
+  for (let n = 0; n < N; ++n) {
+    shapes.forEach((shape, ns) => {
+      if (sizes[ns] > n) {
+        const dimID = sizes[ns] - n - 1 // right to left
+        const nDim = shape[dimID]
+        const broadcastID = N - n - 1 // right to left
+        if ((nDim < maxDims[broadcastID]) & (nDim > 1)) {
+          throw new Error(`shape missmatch: missmatch is found in arg ${ns} with shape (${shape}) not possible to broadcast dimension ${dimID} with size ${nDim} to size ${maxDims[broadcastID]}`)
+        }
+        else {
+          broadcasted_shape[broadcastID] = maxDims[broadcastID]
+        }
+      }
+    })
+  }
+
+  return broadcasted_shape
+}
+
+function broadcast_matrices(...Ms) {
+  // Broadcasts many arrays
+	
+  // if they are all scalars, there is nothing to do
+  if (Ms.every(x => isScalar(x))) {
+    return Ms
+  }
+
+  const s = broadcast_shapes(...Ms.map(x => sizeAsArray(x)))
+  const N = s.length
+  return Ms.map(M => {
+    if (isScalar(M) & !sizeOfScalar(s)) {
+      M = math.matrix([M])
+    }
+
+    if (sizeAsArray(M).length < N) {
+      M.reshape(shapeToConcat(M, N))
+    }
+
+    for (let i = 0; i < N; ++i) {
+      if (((M.size()[i] == 1) | (M.size()[i] == 0)) & (s[i] > 1)) {
+        M = math.concat(...Array(s[i]).fill(M), i)
+      }
+    }
+    return M
+  })
+}
+
+const mat = math.create()
 
 mat.import({
+  add: (...Ms) => math.add(...broadcast_matrices(...Ms)),
+  subtract: (...Ms) => math.subtract(...broadcast_matrices(...Ms)),
+  dotMultiply: (...Ms) => math.dotMultiply(...broadcast_matrices(...Ms)),
+  dotDivide: (...Ms) => math.dotDivide(...broadcast_matrices(...Ms)),
   props,
   HAprops,
   phase,
