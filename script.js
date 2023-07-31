7d050c2e421b7f541e4c41e90e940f0e7f6b5b92
David Contreras <dvd.cnt@gmail.com>
2023-07-31 21:57:40 +0000

    Autocomplete

--- script.js
+++ script.js
@@ -23,6 +23,108 @@ const example = [
   "HAprops('T', {P:1 atm, H:h, R:1.0})"
 ]
 
+/**
+ * auto complete a text
+ * @param {String} text
+ * @return {[Array, String]} completions
+ */
+function completer(text) {
+  // based on https://github.com/josdejong/mathjs/tree/develop/bin/cli.js
+  let matches = []
+  let keyword
+  const m = /[a-zA-Z_0-9]+$/.exec(text)
+  if (m) {
+    keyword = m[0]
+
+    // scope variables
+    for (const def in parser.getAll()) {
+      if (def.indexOf(keyword) === 0) {
+        matches.push(def)
+      }
+    }
+
+    // commandline keywords
+    ['exit', 'quit', 'clear'].forEach(function (cmd) {
+      if (cmd.indexOf(keyword) === 0) {
+        matches.push(cmd)
+      }
+    })
+
+    // math functions and constants
+    const ignore = ['expr', 'type']
+    for (const func in math.expression.mathWithTransform) {
+      if (hasOwnPropertySafe(math.expression.mathWithTransform, func)) {
+        if (func.indexOf(keyword) === 0 && ignore.indexOf(func) === -1) {
+          matches.push(func)
+        }
+      }
+    }
+
+    // units
+    const Unit = math.Unit
+    for (const name in Unit.UNITS) {
+      if (hasOwnPropertySafe(Unit.UNITS, name)) {
+        if (name.indexOf(keyword) === 0) {
+          matches.push(name)
+        }
+      }
+    }
+    for (const name in Unit.PREFIXES) {
+      if (hasOwnPropertySafe(Unit.PREFIXES, name)) {
+        const prefixes = Unit.PREFIXES[name]
+        for (const prefix in prefixes) {
+          if (hasOwnPropertySafe(prefixes, prefix)) {
+            if (prefix.indexOf(keyword) === 0) {
+              matches.push(prefix)
+            } else if (keyword.indexOf(prefix) === 0) {
+              const unitKeyword = keyword.substring(prefix.length)
+              for (const n in Unit.UNITS) {
+                const fullUnit = prefix + n
+                if (hasOwnPropertySafe(Unit.UNITS, n)) {
+                  if (n.indexOf(unitKeyword) === 0 &&
+                    Unit.isValuelessUnit(fullUnit) &&
+                    !matches.includes(fullUnit)) {
+                    matches.push(fullUnit)
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // remove duplicates
+    matches = matches.filter(function (elem, pos, arr) {
+      return arr.indexOf(elem) === pos
+    })
+  }
+
+  return matches
+}
+
+function mathHints(cm, options) {
+  return new Promise(function (accept) {
+    setTimeout(function () {
+      const cursor = cm.getCursor(), line = cm.getLine(cursor.line)
+      let start = cursor.ch, end = cursor.ch
+      while (start && /\w/.test(line.charAt(start - 1))) --start
+      while (end < line.length && /\w/.test(line.charAt(end))) ++end
+      const word = line.slice(start, end).toLowerCase()
+      const results = completer(word)
+      if (results.length > 0) {
+        return accept({
+          list: results,
+          from: CodeMirror.Pos(cursor.line, start),
+          to: CodeMirror.Pos(cursor.line, end)
+        })
+      } else {
+        return accept(null)
+      }
+    }, 100)
+  })
+}
+
 const parser = math.parser()
 const inputCode = document.getElementById('editor')
 inputCode.value = example.join('\n');
@@ -44,6 +146,7 @@ var editor = CodeMirror.fromTextArea(inputCode, {
   showCursorWhenSelecting: true,
   theme: "blackboard",
   styleActiveLine: true,
+  hintOptions: { hint: mathHints }
 });
 
 editor.on("change", code => {
@@ -140,4 +243,10 @@ const results = document.getElementById("output")
 
 function sendMath() {
   results.innerHTML = makeDoc(editor.getValue());
+}
+
+// helper function to safely check whether an object has a property
+// copy from the function in object.js which is ES6
+function hasOwnPropertySafe(object, property) {
+  return object && Object.hasOwnProperty.call(object, property)
 }
